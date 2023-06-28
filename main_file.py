a5962e2813fd2d75e34ea26aaaf17ba163044b90
Thure Højgaard Nielsen <thure.hojgaard.nielsen@gmail.com>
2023-06-28 11:26:20 +0200

    Added comments, new system in progress

--- main_file.py
+++ main_file.py
@@ -4,28 +4,41 @@ import tkinter as tk
 window = tk.Tk()
 window.title("Calculator")
 
-# Define functions
+# Define event functions
 def on_click_button(event):
-    calculation_field["text"] = calculator.process_input(input=event.widget["text"])
+    calculator.process_input(input=event.widget["text"])
+    equation_field["text"] = calculator.display
 
 def on_resized_window(event):
-    calculation_field["width"] = int(window.winfo_width()/9)
+    equation_field["width"] = int(window.winfo_width()/9)
 
-# Main functioning class
+
 class Calculator():
+
+    # Main functinary class
 
     def __init__(self):
 
-        self.state = "digit1"
-        self.display = ""
-        self.valid_states = (
-            "digit1",
-            "operator1",
-            "digit req",
-            "digit2",
-            "digit3+"
-            "calculate"
+        # Equation processed
+        self.equation = [""]
+
+        # Valid digits that will be accepted
+        self.valid_digits = (
+            ".",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "0",
+            "00"
         )
+
+        # Valid operators that will be accepted
         self.valid_operators = (
             "+",
             "-",
@@ -33,40 +46,40 @@ class Calculator():
             "/"
         )
 
+    @property
+    def display(self):
+        # Returns equation as str
+        return "".join(self.equation)
+
     def process_input(self, input):
 
+        # Clear equation
         if input == "C":
-            self.state = "digit1"
-            self.display = ""
-            return self.display
-        elif self.state == "digit1" and input.isdigit():
-            self.state = "operator1"
-            self.display = "".join((self.display, input))
-            return self.display
-        elif self.state == "operator1" and input.isdigit():
-            self.display = "".join((self.display, input))
-            return self.display
-        elif self.state == "operator1" and input in self.valid_operators:
-            self.state = "digit2"
-            self.display = "".join((self.display, input))
-            return self.display
-        elif self.state == "digit2" and input.isdigit():
-            self.state = "calculate"
-            self.display = "".join((self.display, input))
-            return self.display
-        elif self.state == "calculate" and input.isdigit():
-            self.display = "".join((self.display, input))
-            return self.display
-        elif self.state == "calculate" and input in self.valid_operators:
-            self.state = "digit3+"
-            self.display = "".join((self.display, input))
-            return self.display
-        elif self.state == "digit3+" and input.isdigit():
-            self.state = "calculate"
-            self.display = "".join((self.display, input))
-            return self.display
-        elif self.state == "calculate" and input == "=":
-            return eval(self.display)
+            self.equation = [""]
+
+        # Calculate equation
+        elif input == "=":
+            pass
+
+        # Insert digit
+        elif input in self.valid_digits:
+            # Check if equation is valid
+            try:
+                float(self.equation[-1] + input)
+            except ValueError:
+                pass
+            else:
+                self.equation[-1] = self.equation[-1] + input
+
+        # Insert operator
+        elif input in self.valid_operators:
+            if self.equation[-1][-1].isdigit():
+                self.equation.append(input)
+            else:
+                pass
+        else:
+            # Error if input is invalid
+            raise ValueError("Invalid input:", input)
 
 
 # Initialize widgets
@@ -85,13 +98,12 @@ button_texts = {
     (3, 1): "2",
     (3, 2): "3",
     (3, 3): "+",
-    (3, 4): "",
-    (4, 0): "C",
+    (3, 4): "=",
+    (4, 0): "00",
     (4, 1): "0",
     (4, 2): ".",
     (4, 3): "-",
-    (4, 4): "="
-
+    (4, 4): "C"
 }
 
 num_columns = 5
@@ -101,14 +113,14 @@ calculator = Calculator()
 
 field_frame = tk.Frame(relief=tk.SUNKEN, borderwidth=1)
 field_frame.grid(row=0, column=0, columnspan=num_columns)
-calculation_field = tk.Label(
+equation_field = tk.Label(
     master=field_frame,
     text=calculator.display,
     width=33,
     fg="black",
     bg="white"
 )
-calculation_field.grid(row=0, column=0)
+equation_field.grid(row=0, column=0)
 
 for c in range(num_columns):
     window.columnconfigure(c, weight=1, minsize=56)
